4f35960702ca831642dfb66db79d0218d85f9f4e
David <50714711+schnavy@users.noreply.github.com>
2021-08-15 15:47:29 +0200

    added images aber mit CORS problem

--- public/js/main.js
+++ public/js/main.js
@@ -1,48 +1,82 @@
 // console.log(alttexte)
-let body = document.querySelector("body")
+let content = document.querySelector("#col2")
 let r = 0
-let maybetexte = []
-alttexte.forEach(element => {
-    if(element.includes("May") && !element.includes("text")){
-        let string = element.substring(element.indexOf('May')).replace("an image of ","");
-        maybetexte.push(string)
-        console.log(string);
+let editedData = {
+    "alt": [],
+    "url": [],
+}
+
+for (let i = 0; i < texte.length; i++) {
+    if (texte[i].includes("May") && !texte[i].includes("text")) {
+        let string = texte[i].substring(texte[i].indexOf('May')).replace("an image of ", "");
+        editedData.alt.push(string)
+        editedData.url.push(imgurls[i])
     }
-});
+
+}
 
 let textElements = []
 
-for (i = 0; i < maybetexte.length; i++) {
+
+for (i = 0; i < editedData.alt.length; i++) {
 
     textElements[i] = createNeuesElement(
         "p",
-        "p"+i,
+        "p" + i,
         "poem"
     )
-    textElements[i].textContent = maybetexte[i]
-    // body.appendChild(textElements[i]);
+    textElements[i].textContent = editedData.alt[i]
+    // content.appendChild(textElements[i]);
     // console.log(textElements[i]);
 
 }
 
 setInterval(() => {
-    let old = document.querySelector(".poem")
-    if (old) {
-        
-        body.removeChild(old);
-    }
-
-    r = Math.floor(Math.random()*maybetexte.length)
-    console.log(r);
-    
-    body.appendChild(textElements[r]);
+    // let old = document.querySelector(".poem")
+    r = Math.floor(Math.random() * editedData.alt.length)
+    content.prepend(textElements[r]);
+}, 800);
 
 
-}, 1500);
+let title = document.querySelector("#title")
+let col2 = document.querySelector("#col2")
+let hoverimg = document.querySelector("#hoverimg");
+
+
+title.addEventListener("mouseenter", (e) => {
+    toggleInfos("show")
+})
+
+title.addEventListener("mouseleave", (e) => {
+    toggleInfos("hide")
+})
+
+document.addEventListener("mousemove", (e) => {
+
+    showImg(e.x, e.y)
+})
 
 function createNeuesElement(type, id, klasse) {
     let elem = document.createElement(type);
     elem.setAttribute("id", id);
     elem.setAttribute("class", klasse);
     return elem;
+}
+
+function toggleInfos(action) {
+    let infos = document.querySelector(".info")
+    if (action == "show") {
+        infos.style.display = "block"
+    } else {
+        infos.style.display = "none"
+    }
+}
+
+
+
+function showImg(mouseX, mouseY) {
+    hoverimg.style.left = mouseX + "px"
+    hoverimg.style.top = mouseY + "px"
+    hoverimg.src = editedData.url[100]
+    console.log(editedData.url[100]);
 }
--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,22 @@ const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  try {
 
-  let rawdata = fs.readFileSync('poems.json');
-  let texte = JSON.parse(rawdata);
-  console.log(texte);
-  res.render('index', {
-    title: 'Instagram reflects on love',
-    texte: texte
-  });
+    let rawdata = fs.readFileSync('poems.json');
+    let data = JSON.parse(rawdata);
+    let text = data.alt;
+    let img = data.url;
+    console.log(data);
+
+    res.render('index', {
+      title: 'Instagram reflects on love  ',
+      text: text,
+      imgs: img
+    });
+  } catch (error) {
+    console.log(e);
+  }
 });
 
 module.exports = router;
