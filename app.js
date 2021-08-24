82e9619615fe706d9a3eb48c97cf2165facc7463
David <50714711+schnavy@users.noreply.github.com>
2021-08-24 14:50:39 +0200

    .

--- app.js
+++ app.js
@@ -24,9 +24,8 @@ app.use(express.urlencoded({
   extended: false
 }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public/')));
-console.log(path.join(__dirname, 'public/'));
-
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 
 app.use('/', indexRouter);
 
