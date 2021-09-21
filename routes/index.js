15438a7643bc3d7bd6a0f7b8a5e34fa5a79433dd
David <50714711+schnavy@users.noreply.github.com>
2021-09-21 14:43:47 +0200

    xx

--- routes/index.js
+++ routes/index.js
@@ -13,12 +13,29 @@ router.get('/', function (req, res, next) {
     let data = JSON.parse(rawdata);
 
     res.render('index', {
-      title: 'Instagram reflecting on love  ',
+      title: 'Instagram reflecting on #love',
       text: data,
+      exhibitMode: false,
     });
   } catch (error) {
     console.log(e);
   }
 });
+router.get('/exhibit', function (req, res, next) {
+  try {
+
+    let rawdata = fs.readFileSync('poems.json');
+    let data = JSON.parse(rawdata);
+
+    res.render('index', {
+      title: 'Instagram reflecting on #love',
+      text: data,
+      exhibitMode: true,
+    });
+  } catch (error) {
+    console.log(e);
+  }
+});
+
 
 module.exports = router;
